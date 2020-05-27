37615e640cb0af9026c61c82341ae6170d241e24
Julian Braha <julianbraha@gmail.com>
2020-05-26 20:43:31 -0400

    Add register endpoint

---
+++ Register.php
@@ -0,0 +1,44 @@
+<?php
+	$inData = getRequestInfo();
+
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+  $username = $inData["username"];
+	$password = hash('sha256', $inData["password"]);
+
+	$conn = new mysqli("107.180.58.62", "xk5kfy582mtp", "cPan3131#!#!", "cop4331-contacts");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$sql = "INSERT into User (firstName,lastName,username,password) VALUES ('" . $firstName . "','" . $lastName . "','" . $username . "','" . $password . "')";
+
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+
+	returnWithError("");
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
